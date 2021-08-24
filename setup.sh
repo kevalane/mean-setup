a2facbb0dfa59b093bac834f5a1576907320b8be
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-08-24 23:25:16 +0200

    Changed from LTS to current.

--- setup.sh
+++ setup.sh
@@ -12,13 +12,13 @@ Author: Kevin Ramsusson
 echo "Let's start with NPM & NodeJS:"
 
 # Add latest nodejs using nodesource current version
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 
 # Install nodejs, npm
 sudo apt-get install -y nodejs
 
 # NOTE: IF DEBIAN COMMENT LINES ABOVE, UNCOMMENT BELOW
-# curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
+# curl -fsSL https://deb.nodesource.com/setup_current.x | bash -
 # apt-get install -y nodejs
 
 echo "Done with NPM & NodeJS, let's continue..."
