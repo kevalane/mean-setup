60fd29d1cc9eda0e62f4fc548c4841d28265815e
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-08-25 13:49:36 +0200

    Fixed mongodb.

--- setup.sh
+++ setup.sh
@@ -5,11 +5,12 @@ Initializing..."
 echo "
 -----------------------
 Author: Kevin Ramsusson
+-----------------------"
+
+echo "
 -----------------------
-"
-
-
-echo "Let's start with NPM & NodeJS:"
+Let's start with NPM & NodeJS:
+-----------------------"
 
 # Add latest nodejs using nodesource current version
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
@@ -23,3 +24,31 @@ sudo apt-get install -y nodejs
 
 echo "Done with NPM & NodeJS, let's continue..."
 echo "-----------------"
+
+
+echo "
+-----------------------
+   MongoDB
+-----------------------"
+# https://www.digitalocean.com/community/tutorials/how-to-install-mongodb-on-ubuntu-20-04
+
+# Import mongodb key and add to trusted keys
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+
+# Create the /etc/apt/sources.list.d/mongodb-org-4.4.list directory
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+
+# Update server's local packages
+sudo apt-get update
+
+# Install mongodb
+sudo apt-get install mongodb-org
+
+# Start mongodb service
+sudo systemctl start mongod.service
+
+# Check status
+sudo systemctl status mongod
+
+# Running as expected, continue
+sudo systemctl enable mongod
