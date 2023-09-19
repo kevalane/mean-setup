8fbd16f764b42d880406af541b93cc953b4a1389
Kevin Rasmusson <kevin@jkmholding.com>
2023-09-19 14:16:20 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,12 +12,21 @@ echo "
     NPM & NodeJS:
 -----------------------"
 
-# Add latest nodejs using nodesource current version
-# curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+# [OUTDATED] Add version 18 (latest stable, check nodejs.org so it is still correct)
+# curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+# sudo apt-get install -y nodejs
 
-# Add version 18 (latest stable, check nodejs.org so it is still correct)
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
-sudo apt-get install -y nodejs
+# NEW
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt-get update
+sudo apt-get install nodejs -y
 
 echo "
 -----------------------
